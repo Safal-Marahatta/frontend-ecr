6fb74498311e375254eacd078b19103794a470a1
Safal-Marahatta <safalmarahatta123456@gmail.com>
2024-07-25 16:11:38 +0545

    a

---
+++ Dockerfile
@@ -0,0 +1,38 @@
+# Use the official Node.js image as a base
+FROM node:14 as build
+
+# Set working directory
+WORKDIR /app
+
+# Copy package.json and package-lock.json
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install
+
+# Copy project files
+COPY . .
+
+# Build the React app
+RUN npm run build
+
+EXPOSE 80
+
+# Use lightweight Nginx image for serving static files
+FROM nginx:alpine
+
+
+
+# Copy build files from the previous stage
+COPY --from=build /app/build /usr/share/nginx/html
+
+                    #############
+                    # COPY ./nginx_conf/nginx.conf /etc/nginx/nginx.conf
+                    # COPY ./nginx_conf/default.conf /etc/nginx/conf.d/default.conf
+                    ##############
+
+                    # Expose port 80
+EXPOSE 80
+
+# Default command to start Nginx
+CMD ["nginx", "-g", "daemon off;"]
---
+++ src/App.js
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+  const [name, setName] = useState('');
+  const [submitted, setSubmitted] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch('https://api-safal.learn.cloudlaya.com/api/submit', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name }),
+      });
+
+      if (!response.ok) {
+        throw new Error('sorry Failed to submit data');
+      }
+
+      setSubmitted(true);
+      setName('');
+    } catch (error) {
+      console.error('Error submitting the data:', error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    setName(e.target.value);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Hello World
+        </p>
+
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Enter your name"
+            value={name}
+            onChange={handleInputChange}
+            required
+          />
+          <button type="submit">Submit</button>
+        </form>
+
+        {submitted && <p>Data submitted successfully k!</p>}
+      </header>
+    </div>
+  );
+}
+
+export default App;
